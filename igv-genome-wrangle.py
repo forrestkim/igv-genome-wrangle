c0148b722f34f34c232f097d87c63bff71c631af
[forrest kim] <[f1kim@ucsd.edu]>
2018-12-04 09:39:58 -0800

    added overwriting and documentation

--- igv-genome-wrangle.py
+++ igv-genome-wrangle.py
@@ -7,7 +7,8 @@
 # Input genome ID
 genome_id = input("Print Genome ID: ")
 
-#make new dir
+# Create a new directory, if it doesn't exist
+# If identical files exist within the genome id directory, files will be overwritten
 import os
 try:
     os.mkdir(genome_id)
@@ -15,22 +16,22 @@ try:
 except FileExistsError:
     os.chdir(genome_id)
 
-# wget all the necessary files
+# Download and Index necessary files
 import subprocess
 
-# Fasta
+# Fasta download and indexing
 fasta_url = 'http://hgdownload.soe.ucsc.edu/goldenPath/%s/bigZips/%s.fa.gz' % (genome_id, genome_id)
-subprocess.run(['wget', '%s' % (fasta_url)])
+subprocess.run(['wget', '%s' % (fasta_url), '-O', '%s.fa.gz' % (genome_id)])
 subprocess.run(['gunzip','%s.fa.gz' % (genome_id)])
-subprocess.run(['samtools','faidx','%s.fa' % (genome_id)])
+subprocess.run(['samtools','faidx','%s.fa' % (genome_id)]) # indexes fasta
 
-# Cytoband
+# Cytoband download
 cytoband_url = 'http://hgdownload.soe.ucsc.edu/goldenPath/%s/database/cytoBandIdeo.txt.gz' % (genome_id) 
-subprocess.run(['wget', '%s' % (cytoband_url)])
+subprocess.run(['wget', '%s' % (cytoband_url), '-O', 'cytoBandIdeo.txt.gz'])
 
-# Gene
+# Gene download and indexing
 gene_url = 'http://hgdownload.soe.ucsc.edu/goldenPath/%s/database/refGene.txt.gz' % (genome_id)
-subprocess.run(['wget', '%s' % (gene_url)])
+subprocess.run(['wget', '%s' % (gene_url), '-O', 'refGene.txt.gz'])
 subprocess.run(['gunzip','refGene.txt.gz'])
 subprocess.run(['bgzip','refGene.txt'])
-subprocess.run(['tabix','refGene.txt.gz'])
+subprocess.run(['tabix','refGene.txt.gz']) #indexes gene list 
